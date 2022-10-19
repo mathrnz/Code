94158edb4f82ac4b438d55632979a2134c69092a
Baptiste Paqueriaud <63961483+BaptisteP31@users.noreply.github.com>
2022-10-19 14:48:59 +0200

    EX4

---
+++ R1-01/TD/TD05/EX4/String.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+struct String{
+    size_t allocated_size;
+    char *buffer;
+};
+
+void CreateString(String & str, const size_t size=256);
+void DisplayString(const String & str);
+size_t StringLength(const String & str);
+void DestroyString(String & str);
---
+++ R1-01/TD/TD05/EX4/main.cpp
@@ -0,0 +1,7 @@
+#include "String.hpp"
+
+int main(int argc, char *argv[]) {
+    String s1;
+    CreateString(s1, 200);
+    DisplayString(s1);
+}
