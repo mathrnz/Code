0c3524e3b743c5ec14167a53c943eba743a9e564
Baptiste Paqueriaud <63961483+BaptisteP31@users.noreply.github.com>
2022-10-20 14:06:49 +0200

    EX3

---
+++ R1-01/TP/TP09/EX3/EX3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "../../../includes/CommonThings/commonthings.hpp"
+
+void echange2(double *pa, double *pb) {
+    if(*pa > *pb) {
+        double pc=*pa;
+        *pa=*pb;
+        *pb=pc;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    double a, b;
+    commonthings::userinput::askUser(a, "A?");
+    commonthings::userinput::askUser(b, "B?");
+
+    echange2(&a, &b);
+
+    std::cout << "A: " << a << " B: " << b << std::endl;
+
+    return EXIT_SUCCESS;
+}
