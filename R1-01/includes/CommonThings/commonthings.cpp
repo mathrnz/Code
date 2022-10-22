1612649509be29699e22c4ce00c370a98451dc12
Baptiste Paqueriaud <paqueriaudbaba@gmail.com>
2022-10-22 21:14:07 +0200

    updated commonthing

--- R1-01/includes/CommonThings/commonthings.cpp
+++ R1-01/includes/CommonThings/commonthings.cpp
@@ -17,103 +17,417 @@
 +-----------------------------------------------------------------------------------+
 */
 
+#include <limits>
 #include "commonthings.hpp"
 
 namespace commonthings {
     
     namespace userinput {
+        void handleWrongInput(const std::string error) { //Used to handle wrong input
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+            std::cout << error << std::endl;
+        }
+
         //Ask User, references the variable
-        void askUser(unsigned int& n, std::string msg) {
-            std::cout << msg << "> ";
-            std::cin >> n;
+        void askUser(unsigned int& n, std::string msg, const std::string error="Invalid input") { //Reference to variable, string to ask, optional error message
+            while(true) {
+                std::cout << msg << "> ";
+                std::cin >> n;
+
+                if(std::cin.fail())
+                    handleWrongInput(error);
+                else
+                    break;
+            }
         }
-        void askUser(long unsigned int& n, std::string msg) {
-            std::cout << msg << "> ";
-            std::cin >> n;
+        void askUser(long unsigned int& n, std::string msg, const std::string error="Invalid input") { //Reference variable, string to ask, optional error message
+            while(true) {
+                std::cout << msg << "> ";
+                std::cin >> n;
+
+                if(std::cin.fail())
+                    handleWrongInput(error);
+                else
+                    break;
+            }
         }
-        void askUser(long long unsigned int& n, std::string msg) {
-            std::cout << msg << "> ";
-            std::cin >> n;
+        void askUser(long long unsigned int& n, std::string msg, const std::string error="Invalid input") { //Reference variable, string to ask, optional error message
+            while(true) {
+                std::cout << msg << "> ";
+                std::cin >> n;
+
+                if(std::cin.fail())
+                    handleWrongInput(error);
+                else
+                    break;
+            }
         }
 
-        void askUser(int& n, std::string msg) {
-            std::cout << msg << "> ";
-            std::cin >> n;
-        }
-        void askUser(long int& n, std::string msg) {
-            std::cout << msg << "> ";
-            std::cin >> n;
-        }
-        void askUser(long long int& n, std::string msg) {
-            std::cout << msg << "> ";
-            std::cin >> n;
+        void askUser(int& n, std::string msg, const std::string error="Invalid input") { //Reference variable, string to ask, optional error message
+            while(true) {
+                std::cout << msg << "> ";
+                std::cin >> n;
+
+                if(std::cin.fail())
+                    handleWrongInput(error);
+                else
+                    break;
+            }
         }
 
-        void askUser(double& n, std::string msg){ 
-            std::cout << msg << "> ";
-            std::cin >> n;
-        }
-        void askUser(long double& n, std::string msg) {
-            std::cout << msg << "> ";
-            std::cin >> n;
+        void askUser(long int& n, std::string msg, const std::string error="Invalid input") { //Reference variable, string to ask, optional error message
+            while(true) {
+                std::cout << msg << "> ";
+                std::cin >> n;
+
+                if(std::cin.fail())
+                    handleWrongInput(error);
+                else
+                    break;
+            }
         }
 
-        void askUser(float& n, std::string msg) {
-            std::cout << msg << "> ";
-            std::cin >> n;
+        void askUser(long long int& n, std::string msg, const std::string error="Invalid input") { //Reference variable, string to ask, optional error message
+            while(true) {
+                std::cout << msg << "> ";
+                std::cin >> n;
+
+                if(std::cin.fail())
+                    handleWrongInput(error);
+                else
+                    break;
+            }
         }
 
-        void askUser(char& n, std::string msg) {
-            std::cout << msg << "> ";
-            std::cin >> n;
+        void askUser(double& n, std::string msg, const std::string error="Invalid input") { //Reference variable, string to ask, optional error message
+            while(true) {
+                std::cout << msg << "> ";
+                std::cin >> n;
+
+                if(std::cin.fail())
+                    handleWrongInput(error);
+                else
+                    break;
+            }
+        }
+        void askUser(long double& n, std::string msg, const std::string error="Invalid input") { //Reference variable, string to ask, optional error message
+            while(true) {
+                std::cout << msg << "> ";
+                std::cin >> n;
+
+                if(std::cin.fail())
+                    handleWrongInput(error);
+                else
+                    break;
+            }
+        }
+
+        void askUser(float& n, std::string msg, const std::string error="Invalid input") { //Reference variable, string to ask, optional error message
+            while(true) {
+                std::cout << msg << "> ";
+                std::cin >> n;
+
+                if(std::cin.fail())
+                    handleWrongInput(error);
+                else
+                    break;
+            }
+        }
+
+        void askUser(char& n, std::string msg, const std::string error="Invalid input") { //Reference variable, string to ask, optional error message
+            while(true) {
+                std::cout << msg << "> ";
+                std::cin >> n;
+
+                if(std::cin.fail())
+                    handleWrongInput(error);
+                else
+                    break;
+            }
         }
         
-        void askUser(std::string& n, std::string msg) {
-        std::cout << msg << "> ";
-        std::cin >> n;
-    }
+        void askUser(std::string& n, std::string msg, const std::string error="Invalid input") { //Reference variable, string to ask, optional error message
+            while(true) {
+                std::cout << msg << "> ";
+                std::cin >> n;
+
+                if(std::cin.fail())
+                    handleWrongInput(error);
+                else
+                    break;
+            }
+        }
     }
 
     namespace booltest {
     //Test the eveness of a number, return true for even, false for odd
-    bool isEven(unsigned int n) {
-        bool even = false;
-        (n%2==0)?even=true:even=false;
+        bool isEven(unsigned int n) {
+            return (n%2==0);
+        }
 
-        return even;
+        bool isEven(long unsigned int n) {
+            return (n%2==0);
+        }
+
+        bool isEven(long long unsigned int n) {
+            return (n%2==0);
+        }
+
+        bool isEven(int n) {
+            return (n%2==0);
+        }
+
+        bool isEven(long int n) {
+            return (n%2==0);
+        }
+        
+        bool isEven(long long int n) {
+            return (n%2==0);
+        }
+    }
+    
+    namespace calculation {
+    //All calculation functions
+        double average(const double t[], int size) {
+            double sum = 0;
+            
+            for (int i = 0; i < size; ++i)
+                sum += t[i];
+    
+            return sum / (double)size;
+        }
+        
+        double average(const int t[], int size) {
+            double sum = 0;
+            
+            for (int i = 0; i < size; ++i)
+                sum += t[i];
+     
+            return sum / size;
+        }
+        
+        double average(const long  t[], int size) {
+            double sum = 0;
+            
+            for (int i = 0; i < size; ++i)
+                sum += t[i];
+     
+            return sum / size;
+        }
+        
+        double average(const long long t[], int size) {
+            double sum = 0;
+            
+            for (int i = 0; i < size; ++i)
+                sum += t[i];
+     
+            return sum / size;
+        }
+                
+        double average(const unsigned int t[], int size) {
+            double sum = 0;
+            
+            for (int i = 0; i < size; ++i)
+                sum += t[i];
+     
+            return sum / size;
+        }
+
+        double average(const unsigned long t[], int size) {
+            double sum = 0;
+            
+            for (int i = 0; i < size; ++i)
+                sum += t[i];
+     
+            return sum / size;
+        }
+        
+        double average(const unsigned long long t[], int size) {
+            double sum = 0;
+            
+            for (int i = 0; i < size; ++i)
+                sum += t[i];
+     
+            return sum / size;
+        }
+        
+        double average(std::vector<int> t) {
+            double sum = 0;
+            
+            for (int &i : t)
+                sum += i;
+
+            return sum / t.size();
+        }
+        
+        double average(std::vector<double> t) {
+            double sum = 0;
+            
+            for (double &i : t)
+                sum += i;
+
+            return sum / t.size();
+        }
+        
+        double average(std::vector<long> t) {
+            double sum = 0;
+            
+            for (long &i : t)
+                sum += i;
+
+            return sum / t.size();
+        }
+        
+        double average(std::vector<long long> t) {
+            double sum = 0;
+            
+            for (long long &i : t)
+                sum += i;
+
+            return sum / t.size();
+        }
+                
+        double average(std::vector<unsigned int> t) {
+            double sum = 0;
+            
+            for (unsigned &i : t)
+                sum += i;
+
+            return sum / t.size();
+        }
+        
+        double average(std::vector<unsigned long> t) {
+            double sum = 0;
+            
+            for (unsigned long &i : t)
+                sum += i;
+
+            return sum / t.size();
+        }
+        
+        double average(std::vector<unsigned long long> t) {
+            double sum = 0;
+            
+            for (unsigned long long &i : t)
+                sum += i;
+
+            return sum / t.size();
+        }
     }
 
-    bool isEven(long unsigned int n) {
-                bool even = false;
-        (n%2==0)?even=true:even=false;
-        
-        return even;
-    }
+    namespace armanip {
+        //Removes the last item of an array
+        unsigned int popBack(unsigned int tab[], size_t& n){
+            return tab[--n];
+        } 
 
-    bool isEven(long long unsigned int n) {
-                bool even = false;
-        (n%2==0)?even=true:even=false;
-        
-        return even;
-    }
+        long unsigned int popBack(long unsigned int tab[], size_t& n) {
+            return tab[--n];
+        } 
 
-    bool isEven(int n) {
-                bool even = false;
-        (n%2==0)?even=true:even=false;
+        long long unsigned int popBack(long long unsigned int tab[], size_t& n){
+            return tab[--n];
+        } 
+
+        int popBack(int tab[], size_t& n) {
+            return tab[--n];
+        } 
+            
+        long int popBack(long int tab[], size_t& n) {
+            return tab[--n];
+        } 
         
-        return even;
-    }
-    bool isEven(long int n) {
-                bool even = false;
-        (n%2==0)?even=true:even=false;
-        
-        return even;
-    }
-    bool isEven(long long int n) {
-                bool even = false;
-        (n%2==0)?even=true:even=false;
-        
-        return even;
-    }
+        long long int popBack(long long int tab[], size_t& n){
+            return tab[--n];
+        } 
+
+        double popBack(double tab[], size_t& n){
+            return tab[--n];
+        } 
+
+        long double popBack(long double tab[], size_t& n){
+            return tab[--n];
+        } 
+
+        float popBack(float tab[], size_t& n){
+            return tab[--n];
+        } 
+
+        char popBack(char tab[], size_t& n){
+            return tab[--n];
+        } 
+
+        std::string popBack(std::string tab[], size_t& n){
+            return tab[--n];
+        }
+
+        //Removes the first item of an array
+        unsigned int popFront(unsigned int tab[], size_t& n) {
+            for(size_t i=1; i<n; i++)
+                tab[i-1]=tab[i];
+            return tab[--n]; 
+        }
+
+        long unsigned int popFront(long unsigned int tab[], size_t& n) {
+            for(size_t i=1; i<n; i++)
+                tab[i-1]=tab[i];
+            return tab[--n]; 
+        }
+
+        long long unsigned int popFront(long long unsigned int tab[], size_t& n) {
+            for(size_t i=1; i<n; i++)
+                tab[i-1]=tab[i];
+            return tab[--n]; 
+        }
+
+        int popFront(int tab[], size_t& n) {
+            for(size_t i=1; i<n; i++)
+                tab[i-1]=tab[i];
+            return tab[--n]; 
+        }
+
+        long int popFront(long int tab[], size_t& n) {
+            for(size_t i=1; i<n; i++)
+                tab[i-1]=tab[i];
+            return tab[--n]; 
+        }
+
+        long long int popFront(long long int tab[], size_t& n) {
+            for(size_t i=1; i<n; i++)
+                tab[i-1]=tab[i];
+            return tab[--n]; 
+        }
+
+        double popFront(double tab[], size_t& n) {
+            for(size_t i=1; i<n; i++)
+                tab[i-1]=tab[i];
+            return tab[--n]; 
+        }
+
+        long double popFront(long double tab[], size_t& n) {
+            for(size_t i=1; i<n; i++)
+                tab[i-1]=tab[i];
+            return tab[--n]; 
+        }
+
+        float popFront(float tab[], size_t& n) {
+            for(size_t i=1; i<n; i++)
+                tab[i-1]=tab[i];
+            return tab[--n]; 
+        }
+
+        char popFront(char tab[], size_t& n) {
+            for(size_t i=1; i<n; i++)
+                tab[i-1]=tab[i];
+            return tab[--n]; 
+        }
+
+        std::string popFront(std::string tab[], size_t& n) {
+            for(size_t i=1; i<n; i++)
+                tab[i-1]=tab[i];
+            return tab[--n]; 
+        }
     }
 }
+
--- R1-01/includes/CommonThings/commonthings.hpp
+++ R1-01/includes/CommonThings/commonthings.hpp
@@ -22,41 +22,102 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 namespace commonthings {
 
     namespace userinput {
-    //Ask User, references the variable
-    void askUser(unsigned int& n, std::string msg);
-    void askUser(long unsigned int& n, std::string msg);
-    void askUser(long long unsigned int& n, std::string msg);
+        
+        void askUser(unsigned int& n, std::string msg, const std::string error="Invalid input"); //Reference to variable, string to ask, optional error message
+        void askUser(long unsigned int& n, std::string msg, const std::string error="Invalid input"); //Reference to variable, string to ask, optional error message
+        void askUser(long long unsigned int& n, std::string msg, const std::string error="Invalid input"); //Reference to variable, string to ask, optional error message
 
-    void askUser(int& n, std::string msg);
-    void askUser(long int& n, std::string msg);
-    void askUser(long long int& n, std::string msg);
+        void askUser(int& n, std::string msg, const std::string error="Invalid input"); //Reference to variable, string to ask, optional error message
+        void askUser(long int& n, std::string msg, const std::string error="Invalid input"); //Reference to variable, string to ask, optional error message
+        void askUser(long long int& n, std::string msg, const std::string error="Invalid input"); //Reference to variable, string to ask, optional error message
 
-    void askUser(double& n, std::string msg);
-    void askUser(long double& n, std::string msg);
+        void askUser(double& n, std::string msg, const std::string error="Invalid input"); //Reference to variable, string to ask, optional error message
+        void askUser(long double& n, std::string msg, const std::string error="Invalid input"); //Reference to variable, string to ask, optional error message
 
-    void askUser(float& n, std::string msg);
+        void askUser(float& n, std::string msg, const std::string error="Invalid input"); //Reference to variable, string to ask, optional error message
 
-    void askUser(char& n, std::string msg);
-    
-    void askUser(std::string& n, std::string msg);
+        void askUser(char& n, std::string msg, const std::string error="Invalid input"); //Reference to variable, string to ask, optional error message
+        
+        void askUser(std::string& n, std::string msg, const std::string error="Invalid input"); //Reference to variable, string to ask, optional error message
     }
 
     namespace bootest {
-    //Test the eveness of a number, return true for even, false for odd
-    bool isEven(unsigned int n);
-    bool isEven(long unsigned int n);
-    bool isEven(long long unsigned int n);
+    
+        bool isEven(unsigned int n); //Test the eveness of a number, return true for even, false for odd
+        bool isEven(long unsigned int n); //Test the eveness of a number, return true for even, false for odd
+        bool isEven(long long unsigned int n); //Test the eveness of a number, return true for even, false for odd
 
-    bool isEven(int n);
-    bool isEven(long int n);
-    bool isEven(long long int n);
+        bool isEven(int n); //Test the eveness of a number, return true for even, false for odd
+        bool isEven(long int n); //Test the eveness of a number, return true for even, false for odd
+        bool isEven(long long int n); //Test the eveness of a number, return true for even, false for odd
+    }
+    
+    namespace calculation {
+        double average(const double t[], int size); //Returns the average of the content of an array of double
+        double average(const int t[], int size); //Returns the average of the content of an array of int
+        
+        double average(const long t[], int size); //Returns the average of the content of an array of long int
+        double average(const long long t[], int size); //Returns the average of the content of an array of long long int
+        
+        double average(const unsigned int t[], int size); //Returns the average of the content of an array of unsigned int
+        double average(const unsigned long t[], int size); //Returns the average of the content of an array of unsigned long int
+        double average(const unsigned long long t[], int size); //Returns the average of the content of an array of unsigned long long int
+        
+        
+        double average(const std::vector<int> t); //Returns the average of the content of a vector of int
+        double average(const std::vector<double> t); //Returns the average of the content of a vector of double
+        
+        double average(const std::vector<long> t); //Returns the average of the content of an vector of long int
+        double average(const std::vector<long long> t); //Returns the average of the content of an vector of long long int
+        
+        double average(const std::vector<unsigned> t); //Returns the average of the content of an vector of unsigned int
+        double average(const std::vector<unsigned long> t); //Returns the average of the content of an vector of unsigned long int
+        double average(const std::vector<unsigned long long> t); //Returns the average of the content of an vector of unsigned long long int
     }
 
-}
+    namespace armanip {
+        //Fonctions used to manipulate arrays
+        unsigned int popBack(unsigned int tab[], size_t& n); //Removes the last item of an array
+        long unsigned int popBack(long unsigned int tab[], size_t& n); //Removes the last item of an array
+        long long unsigned int popBack(long long unsigned int tab[], size_t& n); //Removes the last item of an array
 
+        int popBack(int tab[], size_t& n); //Removes the last item of an array
+        long int popBack(long int tab[], size_t& n); //Removes the last item of an array
+        long long int popBack(long long int tab[], size_t& n); //Removes the last item of an array
+
+        double popBack(double tab[], size_t& n); //Removes the last item of an array
+        long double popBack(long double tab[], size_t& n); //Removes the last item of an array
+
+        float popBack(float tab[], size_t& n); //Removes the last item of an array
+
+        char popBack(char tab[], size_t& n); //Removes the last item of an array
+
+        std::string popBack(std::string tab[], size_t& n); //Removes the last item of an array
+
+
+        unsigned int popFront(unsigned int tab[], size_t& n); //Removes the first item of an array
+        long unsigned int popFront(long unsigned int tab[], size_t& n); //Removes the first item of an array
+        long long unsigned int popFront(long long unsigned int tab[], size_t& n); //Removes the first item of an array
+
+        int popFront(int tab[], size_t& n); //Removes the first item of an array
+        long int popFront(long int tab[], size_t& n); //Removes the first item of an array
+        long long int popFront(long long int tab[], size_t& n); //Removes the first item of an array
+
+        double popFront(double tab[], size_t& n); //Removes the first item of an array
+        long double popFront(long double tab[], size_t& n); //Removes the first item of an array
+
+        float popFront(float tab[], size_t& n); //Removes the first item of an array
+
+        char popFront(char tab[], size_t& n); //Removes the first item of an array
+
+        std::string popFront(std::string tab[], size_t& n); //Removes the first item of an array
+
+    }
+}
 
 #endif
